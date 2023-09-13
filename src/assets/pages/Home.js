4808195830fa66bac3255720e010c2a218644120
Nyasha Hunter <nyashatanganyika21@gmail.com>
2023-09-14 01:15:25 +0200

    master

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import MiniNav from "./MiniNav";
 
 import { Button } from "primereact/button";
@@ -8,12 +9,13 @@ const Home = () => {
 
   return (
     <>
-
       <div className="banner">
         <div className="container text-center">
-          <h1 >A full stack-developer</h1>
+          <h1>A full stack-developer</h1>
           <p className="mb-5">Specialising in JS. C#.React. JS</p>
-          <Button label="Download CV" className="text-center" />
+          <NavLink to="https://docs.google.com/document/d/1-iei2I7q0gNEXzPt9_7eajAQJA1_hAyiWoKLanAofko/edit?usp=sharing">
+            <Button label="Download CV" className="text-center" />
+          </NavLink>
         </div>
         {/* <div className="py-5"></div> */}
         <MiniNav />
